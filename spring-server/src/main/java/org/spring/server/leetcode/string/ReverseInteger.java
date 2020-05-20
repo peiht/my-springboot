7b9eeee1735091de352423704b64698edd4d05b7
peiht <1071532941@qq.com>
2020-05-20 23:08:16 +0800

    holly shit

--- spring-server/src/main/java/org/spring/server/leetcode/string/ReverseInteger.java
+++ spring-server/src/main/java/org/spring/server/leetcode/string/ReverseInteger.java
@@ -35,29 +35,65 @@ public class ReverseInteger {
         }
         //转换成数组
         List<Integer> result = this.getArray(x);
+        if (result == null){
+            return 0;
+        }
         //交换高低位的位置
         int num = 0;
+        int j = 0;
         for (int i=result.size()-1; i>=0; i--){
-            num = num + result.get(i) * 10;
+            int y = (int) Math.pow(10, j);
+            num = num + result.get(i) * y;
+            j++;
         }
-        return 0;
+        if (tar == 1){
+            return -num;
+        }
+        return num;
     }
 
+    //TODO 数组越界待优化
     private List<Integer> getArray(int x){
         List<Integer> list = new ArrayList<>();
         int  count = 0;
-        while (x > 10){
+        int temp = x;
+        while (temp > 0){
             int n = x%10;
             list.add(n);
-            x = x/10;
+            temp = temp/10;
             count = count +1;
+            if (x>214748364 || (x==214748364 && n>7)) { //整数最大的尾数是7，如果>7则溢出返回0
+                return null;
+            }
+            //判断是否 小于 最小32位整数
+            if (x<-214748364 || (x==-214748364 && n<-8)) {//整数最小的尾数是-8，如果<-8则溢出返回0
+                return null;
+            }
         }
         return list;
     }
 
+
+    public int reverse1(int x) {
+        int res = 0;//储存反转值
+        while(x!=0) {
+            int tmp = x % 10; //每次取末尾数字
+            x = x / 10; //每次除以10向前移动移位
+            //判断是否 大于 最大32位整数
+            if (res>214748364 || (res==214748364 && tmp>7)) { //整数最大的尾数是7，如果>7则溢出返回0
+                return 0;
+            }
+            //判断是否 小于 最小32位整数
+            if (res<-214748364 || (res==-214748364 && tmp<-8)) {//整数最小的尾数是-8，如果<-8则溢出返回0
+                return 0;
+            }
+            res = res * 10 + tmp;//乘10向后移位+上取余的值
+        }
+        return res;
+    }
     public static void main(String[] args) {
         ReverseInteger reverseInteger = new ReverseInteger();
-        Integer x = Integer.valueOf(123456);
-        System.out.println(Integer.reverse(x));
+        Integer x = Integer.valueOf(1534236469);
+        System.out.println(reverseInteger.reverse(x));
     }
 }
