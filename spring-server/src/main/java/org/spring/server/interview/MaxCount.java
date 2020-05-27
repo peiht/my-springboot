10014a48739e166b05ab2df9b1fd23b1a9f89c70
peihaitao <peihaitao@duocaitou.com>
2020-05-27 11:42:22 +0800

    update

---
+++ spring-server/src/main/java/org/spring/server/interview/MaxCount.java
@@ -0,0 +1,59 @@
+package org.spring.server.interview;
+
+import java.util.*;
+
+public class MaxCount {
+
+    public int maxCount(int[] array){
+        if (array.length == 0){
+            return 0;
+        }
+
+        int max = 0;
+        for (int i=0; i<array.length; i++){
+            for (int j=i; j<array.length;j++){
+                int[] arrayNew = Arrays.copyOfRange(array, i,j+1);
+                int result = Arrays.stream(arrayNew).sum();
+                if (result > max){
+                    max = result;
+                }
+            }
+        }
+        return max;
+    }
+
+    public int maxCount1(int[] array){
+        if (array.length == 0){
+            return 0;
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for (int i=0; i<array.length; i++){
+            int num = array[i];
+            if (num < 0){
+                list.add(i);
+            }
+        }
+
+        if (list.size() ==0){
+            return Arrays.stream(array).sum();
+        }
+
+        int max = 0;
+        int start = 0;
+        for (int index : list){
+            int[] arrayNew = Arrays.copyOfRange(array, start,index);
+            int result = Arrays.stream(arrayNew).sum();
+            if (result > max){
+                max = result;
+            }
+            start = index+1;
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int[] array = {1, 4, -5, 9, 8, 3, -6};
+        MaxCount maxCount = new MaxCount();
+        System.out.println(maxCount.maxCount1(array));
+    }
+}
---
+++ spring-server/src/main/java/org/spring/server/leetcode/FindDuplicate.java
@@ -0,0 +1,76 @@
+package org.spring.server.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个包含 n + 1 个整数的数组 nums，其数字都在 1 到 n 之间（包括 1 和 n），可知至少存在一个重复的整数。假设只有一个重复的整数，找出这个重复的数。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,3,4,2,2]
+ * 输出: 2
+ * 示例 2:
+ *
+ * 输入: [3,1,3,4,2]
+ * 输出: 3
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-the-duplicate-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author hitopei
+ *
+ */
+public class FindDuplicate {
+
+    public int findDuplicate(int[] nums){
+        if (nums.length == 0){
+            return -1;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums){
+            Integer value = map.getOrDefault(num, 0);
+            if (value > 0){
+                return num;
+            }
+            map.put(num, 1);
+        }
+        return 0;
+    }
+
+    public int findDuplicate1(int[] nums) {
+        int len = nums.length;
+        int left = 1;
+        int right = len - 1;
+        while (left < right) {
+            // 在 Java 里可以这么用，当 left + right 溢出的时候，无符号右移保证结果依然正确
+            int mid = (left + right) >>> 1;
+
+            int cnt = 0;
+            for (int num : nums) {
+                if (num <= mid) {
+                    cnt += 1;
+                }
+            }
+
+            // 根据抽屉原理，小于等于 4 的个数如果严格大于 4 个
+            // 此时重复元素一定出现在 [1, 4] 区间里
+            if (cnt > mid) {
+                // 重复元素位于区间 [left, mid]
+                right = mid;
+            } else {
+                // if 分析正确了以后，else 搜索的区间就是 if 的反面
+                // [mid + 1, right]
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,3,4,2,2};
+        FindDuplicate findDuplicate = new FindDuplicate();
+        System.out.println(findDuplicate.findDuplicate(nums));
+    }
+}
