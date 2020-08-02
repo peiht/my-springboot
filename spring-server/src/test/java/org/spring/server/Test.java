00370db528e61561f7f9e8937893e29924c983fc
peihaitao <peihaitao@duocaitou.com>
2020-08-02 22:59:28 +0800

    update
    Co-Authored-By: peiht <1071532941@qq.com>

--- spring-server/src/test/java/org/spring/server/Test.java
+++ spring-server/src/test/java/org/spring/server/Test.java
@@ -1,9 +1,14 @@
 package org.spring.server;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Test {
     public static void main(String[] args) {
-        ConcurrentHashMap concurrentHashMap = new ConcurrentHashMap();
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        queue.poll();
+
     }
 }
