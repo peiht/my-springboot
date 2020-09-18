4a62c01c928dac69ce84cc9201d10fb61d0f2d67
peihaitao <peihaitao@duocaitou.com>
2020-09-18 10:33:36 +0800

    每日更新
    Co-Authored-By: peiht <1071532941@qq.com>

---
+++ spring-server/src/main/java/org/spring/server/leetcode/array/PermuteUnique.java
@@ -0,0 +1,62 @@
+package org.spring.server.leetcode.array;
+
+import java.util.*;
+
+/**
+ * @author hitopei
+ *
+ *给定一个可包含重复数字的序列，返回所有不重复的全排列。
+ *
+ * 示例:
+ *
+ * 输入: [1,1,2]
+ * 输出:
+ * [
+ *   [1,1,2],
+ *   [1,2,1],
+ *   [2,1,1]
+ * ]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/permutations-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class PermuteUnique {
+    public List<List<Integer>> permuteUnique(int[] nums){
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums.length == 0) {
+            return res;
+        }
+
+        Arrays.sort(nums);
+        int len = nums.length;
+        Deque<Integer> path = new ArrayDeque<>();
+        boolean[] used = new boolean[len];
+        dfs(nums, 0, res, path, len, used);
+        return res;
+    }
+
+    private void dfs(int nums[], int index, List<List<Integer>> res, Deque<Integer> path, int length, boolean[] used) {
+        if (index >= length) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = 0; i < length; i++) {
+            if (used[i] || (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])) {
+                continue;
+            }
+
+            path.addLast(nums[i]);
+            used[i] = true;
+            dfs(nums, index + 1, res, path, length, used);
+            used[i] = false;
+            path.removeLast();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2};
+        System.out.println(new PermuteUnique().permuteUnique(nums));
+    }
+}
