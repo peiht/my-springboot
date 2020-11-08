8d37aea56a175838f0c7b014e6c4c400d8abc1df
peihaitao <peihaitao@duocaitou.com>
2020-11-08 09:58:45 +0800

    leetcode
    Co-Authored-By: peiht <30573549+peiht@users.noreply.github.com>

---
+++ spring-server/src/main/java/org/spring/server/leetcode/array/ValidMountainArray.java
@@ -0,0 +1,43 @@
+package org.spring.server.leetcode.array;
+
+/**
+ * @author hitopei
+ *
+ * 给定一个整数数组 A，如果它是有效的山脉数组就返回 true，否则返回 false。
+ *
+ * 让我们回顾一下，如果 A 满足下述条件，那么它是一个山脉数组：
+ *
+ * A.length >= 3
+ * 在 0 < i < A.length - 1 条件下，存在 i 使得：
+ * A[0] < A[1] < ... A[i-1] < A[i]
+ * A[i] > A[i+1] > ... > A[A.length - 1]
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-mountain-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class ValidMountainArray {
+
+    public boolean validMountainArray(int[] A){
+        int N = A.length;
+        int i = 0;
+
+        // 递增扫描
+        while (i + 1 < N && A[i] < A[i + 1]) {
+            i++;
+        }
+
+        // 最高点不能是数组的第一个位置或最后一个位置
+        if (i == 0 || i == N - 1) {
+            return false;
+        }
+
+        // 递减扫描
+        while (i + 1 < N && A[i] > A[i + 1]) {
+            i++;
+        }
+
+        return i == N - 1;
+    }
+}
--- spring-server/src/test/java/org/spring/server/Test.java
+++ spring-server/src/test/java/org/spring/server/Test.java
@@ -3,6 +3,6 @@ package org.spring.server;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(1234);
+
     }
 }
