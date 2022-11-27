a0cc3d99e3a24f017ff2ab1c54090f09fca8e342
peiht <1071532941@qq.com>
2022-11-27 20:43:06 +0800

    resolve mvn package err

--- design-pattern/src/main/java/org/spring/design/pattern/observer/ObserverDemo.java
+++ design-pattern/src/main/java/org/spring/design/pattern/observer/ObserverDemo.java
@@ -6,12 +6,12 @@ package org.spring.design.pattern.observer;
  */
 public class ObserverDemo {
 
-    public static void main(String[] args) {
-        Subject subject = new Subject();
-
-        new BinaryObserver(subject);
-        new OctalObserver(subject);
-
-        subject.setState(1);
-    }
+//    public static void main(String[] args) {
+//        Subject subject = new Subject();
+//
+//        new BinaryObserver(subject);
+//        new OctalObserver(subject);
+//
+//        subject.setState(1);
+//    }
 }
--- design-pattern/src/main/java/org/spring/design/pattern/strategy/App.java
+++ design-pattern/src/main/java/org/spring/design/pattern/strategy/App.java
@@ -1,11 +1,6 @@
 package org.spring.design.pattern.strategy;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class App {
 
-    public static void main(String[] args) {
-
-    }
 }
